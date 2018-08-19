e02b4755c03804e8077d239fdbab3517eabf2f44
Jay Steele <36495066+JaySteele1@users.noreply.github.com>
2018-08-19 12:10:43 -0600

    Create array.js

---
+++ array.js
@@ -0,0 +1,22 @@
+// Here i put the variables outside the function making them global, just for practice. I could've put all
+// the variables in the function which would make them local to this shitTalk() function only.
+var randomCities = ["Chicago", "Toledo", "Denver", "Westminster", "Detroit"];
+var randomAdjectives = ["pretty", "a dump", "crowded", "quiet", "crime loaded"];
+var randomCity = randomCities[Math.floor(Math.random() * 5)]; // Can use the .length also see below
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * randomAdjectives.length)]; // Can use the *5 also see above
+
+function random() {
+    document.write(randomCity + " is " + randomAdjective + " dont you think?");
+};
+random();
+
+
+// Here is the same code just not as a function, again just for practice. Both have the same random results.
+/*
+var randomCities = ["Chicago", "Toledo", "Denver", "Westminster", "Detroit"];
+var randomAdjectives = ["pretty", "a dump", "crowded", "quiet", "crime loaded"];
+var randomCity = randomCities[Math.floor(Math.random() * 5)];
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * randomAdjectives.length)];
+var randomShitTalk = (randomCity + " is " + randomAdjective + " dont you think?");
+document.write(randomShitTalk);
+*/
